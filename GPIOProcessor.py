17ee541c95464e96a345a4bd8ee5dd67b066646e
Gustavo Bertoli <gubertoli@gmail.com>
2017-06-13 22:26:20 -0300

    Create GPIOProcessor.py
    PIN Assignment according do Linaro/Debian Linux

--- GPIOProcessor.py
+++ GPIOProcessor.py
@@ -12,30 +12,42 @@ class GPIOProcessor:
         self.GPIOList.append(pin)
         return pin
 
-    def getPin21(self):
-        return self.getPin(6)
-
-    def getPin22(self):
-        return self.getPin(7)
-
     def getPin23(self):
-        return self.getPin(36)  #Debian
+        return self.getPin(36)
 
     def getPin24(self):
-        return self.getPin(207)
+        return self.getPin(12)
 
     def getPin25(self):
-        return self.getPin(186)
+        return self.getPin(13)  
 
     def getPin26(self):
-        return self.getPin(189)
+        return self.getPin(69)
 
     def getPin27(self):
-        return self.getPin(22)
+        return self.getPin(115)
 
     def getPin28(self):
-        return self.getPin(23)
+        return self.getPin(4)   #MPP
 
+    def getPin29(self):
+        return self.getPin(24)
+
+    def getPin30(self):
+        return self.getPin(25)
+    
+    def getPin31(self):
+        return self.getPin(35)
+    
+    def getPin32(self):
+        return self.getPin(34)
+
+    def getPin33(self):
+        return self.getPin(28)
+    
+    def getPin34(self):
+        return self.getPin(33)
+    
     def cleanup(self):
         for pin in self.GPIOList:
             pin.input()
